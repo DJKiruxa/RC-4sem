cf456e750e01e7d402ccec6c9cc669d2fe9b8cc9
DJKiruxa <Kirillarhipov0609@mail.ru>
2022-06-07 11:54:48 +0300

    Final

--- app.js
+++ app.js
@@ -5,7 +5,47 @@ const urlencodedParser = express.urlencoded({extended: false});
   
 app.post("/download", urlencodedParser, function (request, response) {
 
+    if(!request.body) {
+        return response.sendStatus(400);
     
+    }
+    console.log(request.body);
+
+	if(typeof request.body.playerName != "string"){     
+    let json = {
+        stattus: 'error',
+        message: "Inccorrect type of data: playerName"
+    };
+    response.end(JSON.stringify(json));
+    return response.sendStatus(400);
+    }
+
+    if(typeof request.body.playerPosition != "string"){     
+    let json = {
+        status: 'error',
+        message: "Inccorrect type of data: playerPosition"
+    };
+    response.end(JSON.stringify(json));
+    return response.sendStatus(400);
+    }
+    
+    if(typeof Number(request.body.playerNumber) != "number"){    
+    let json = {
+        status: 'error',
+        message: "Inccorrect type of data: playerNumber"
+    };
+    response.end(JSON.stringify(json));
+    return response.sendStatus(400);
+    }
+
+    if((!request.body.playerName) || (!request.body.playerPosition) || (!request.body.playerPosition)){    
+    let json = {
+        status: 'error',
+        message: "Not enough data"
+    };
+    response.end(JSON.stringify(json));
+    return response.sendStatus(400);
+    }
 
     let json = {
         name: 'Lacross',
